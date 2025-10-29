fdc1ac5b307ab2059ec4bf087764ce49a4faabd6
Lucio Baiocchi <lucio.baiocchi@studio.unibo.it>
2025-10-29 21:48:24 +0100

    added skeleton of project and calculator test folder

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# agentic-code-demo
-Toy version of claude code, developed following a boot.dev's course
+### Prerequisites 
+
+- Python 3.10+ installed (see the bookbot project for help if you don't already have it)
+- uv project and package manager (download on https://github.comastral-sh/uv)
+- Access to a Unix-like shell (e.g. zsh or bash)
+
+### Startup
+
+Create a virtual environment at the top level of your project directory:
+
+```bash
+uv venv
+```
+
+Activate the env
+```bash
+source .venv/bin/activate
+```
+
+Add dependencies:
+```bash
+uv add google-genai==1.12.1
+uv add python-dotenv==1.1.0
+```
+
+launch main:
+```bash 
+uv run main.py
+```
---
+++ main.py
@@ -0,0 +1,67 @@
+import os
+from google.genai import types
+from google import genai
+from dotenv import load_dotenv
+import sys
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+client = genai.Client(api_key=api_key)
+verbose = False
+
+def main():
+    global verbose
+    pop = sys.argv.pop() 
+    if (pop == "--verbose"):
+        verbose = True
+        user_prompt = sys.argv.pop()
+        print(user_prompt)
+    else:
+        user_prompt = pop
+
+    #print("Digitare -q per uscire")
+    #print("user:", end="")
+    #user_prompt = input()
+    #print("\n")
+    messages = [
+            types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+        ]
+    response = client.models.generate_content(
+        model='gemini-2.0-flash-001', contents=messages
+    )
+        
+    printResponse(response, user_prompt)
+        
+    '''while user_prompt!= '-q':
+        messages = [
+            types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+        ]
+        response = client.models.generate_content(
+            model='gemini-2.0-flash-001', contents=messages
+        )
+        
+        printResponse(response, user_prompt)
+        user_prompt = input()
+    '''
+    #print("\nSOME STATS:\n")
+    #print("Prompt tokens: " + str(response.usage_metadata.prompt_token_count))
+    #print("Response tokens: " + str(response.usage_metadata.candidates_token_count))
+
+def printResponse(response, user_prompt):
+    if (verbose):
+        print("User prompt: " + user_prompt)
+        print("Prompt tokens: " + str(response.usage_metadata.prompt_token_count))
+        print("Response tokens: " + str(response.usage_metadata.candidates_token_count))
+        print("Model Response:" + response.text)
+        #print("user:", end="")
+    else:
+        print("model:" + response.text, end="")
+        #print("user:", end="")
+    
+
+    
+    
+    
+
+if __name__ == "__main__":
+    main()
